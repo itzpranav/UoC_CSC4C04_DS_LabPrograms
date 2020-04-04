1047261689c90f587caf8bbfa5bf18aeb5770509
PRANAV V <itzpranav.v@gmail.com>
2020-04-04 23:49:44 +0530

    Update 17_stackUsingLinkedList.c

--- 17_stackUsingLinkedList.c
+++ 17_stackUsingLinkedList.c
@@ -1,3 +1,54 @@
+/*
+Aim
+
+Implement stack using linkedlist
+
+Algorithm
+
+		1. Start
+		2. Globally declare integer val stuct node pointer next inside struct node with pointer head/top
+		3. Initialize choice as 0
+		4. Print “Stack operations using linked list”.
+		5. Read choice
+		6. If choice=1 Read the value to int val ,call push(val)
+		7. If choice=2 call pop()
+		8. If choice=3 call show()
+		9. If choice=4, then Exit.
+Procedure:-
+	push()
+		1. Start
+		2. Allocate size of struct node for struct node newnode
+		3. Set newnode`s data as val
+		4. If head is null goto step 5 else step 6
+		5. Set newnode`s pointer next as null
+		6. Else Set newnode`s pointer next as head
+		7. Assign value of newnode to head/top
+		8. Print the Success message
+		9. Stop
+	pop()
+		1. Start
+		2. If head is null goto sterp 3 else step 4
+		3. Return Underflow
+		4. Set tempnode as top
+		5. Print the Poped item
+		7. Set head as next of temp node
+		9. free(ptr)
+		[End of loop]
+		10. stop
+	show()
+		1. if top is null goto step 2 else 3
+		2. Return stack is empty
+		3. Declare temperory node and Initialize as top
+		4. While temp next isnt not null repeat step 5 and 6
+		5. Print value of val of temp points to val
+		6. Move pointer of temp to next
+		7. Print value of val of last node
+		8. Stop.
+
+
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -66,7 +117,7 @@ void display()
 	{
 	 printf(" | %d |--->",temp->data);
 	 temp=temp->next;
-      }
-      printf("| %d | ",temp->data);
-   }
+      	}
+	   printf("| %d | ",temp->data);		//for bottom element
+       }
 }
